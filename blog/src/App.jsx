58117010160d11865734cf849fda2c2aa9ac375a
Seungho1201 <ohseungho1201@gmail.com>
2025-02-17 23:04:23 +0900

    useState

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -9,16 +9,42 @@ import './App.css'
 
 function App() {
 
+  // 자료 잠깐 저장할 땐 변수
+  // React에선 state 
   let post = '테스트 변수';
 
+  // 1. import{userState}
+  // 2. useState{보관할 자료}
+  // 3. let[작명, 작명]
+  // 두 번째 작명은 state 변경도와주는 함수
+  // state 변경시 state 쓰던 html은 자동 재렌더링됨
+  // state 언제 씀? -> 변경시 자동으로 html에 반영되게 만들고 싶을 때
+  let[a, b] = useState(['남자 코트 추천', '강남 우동맛집', '리액트 연습']);
+
+  
+  // return 안에는 병렬로 태그 2개 이상 기입금지
   return (
       <div className='App'>
+
         <div className='black-nav'>
           <h4>블로그 글 제목</h4>
         </div>
 
-        <h4 style={ {color : 'red', fontSize:'16px'} }> { post } </h4>
+        <div className="list">
+          <h4>{ a[0] }</h4>
+          <p>2월 17일 발행</p>
+        </div>
+
+        <div className="list">
+          <h4>{ a[1] }</h4>
+          <p>2월 17일 발행</p>
+        </div>
        
+        <div className="list">
+          <h4>{ a[2] }</h4>
+          <p>2월 17일 발행</p>
+        </div>
+
       </div>
 
   );
