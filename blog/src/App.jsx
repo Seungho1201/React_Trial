b6a23553fb8bad6562b8389e9412deeab334a4b3
Seungho1201 <ohseungho1201@gmail.com>
2025-02-17 23:34:41 +0900

    state 변경

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -21,7 +21,18 @@ function App() {
   // state 언제 씀? -> 변경시 자동으로 html에 반영되게 만들고 싶을 때
   let[a, b] = useState(['남자 코트 추천', '강남 우동맛집', '리액트 연습']);
 
-  
+  let[good, goodSet] = useState(0);
+
+  // onClick 안에는 함수 이름만만
+  function goodCount(){
+    console.log(1);
+    goodSet(good + 1);
+  }
+
+  function changeName(){
+    b(['남자 코트 추천', '여자 코트 추천', '리액트 연습']);
+  }
+
   // return 안에는 병렬로 태그 2개 이상 기입금지
   return (
       <div className='App'>
@@ -31,12 +42,12 @@ function App() {
         </div>
 
         <div className="list">
-          <h4>{ a[0] }</h4>
+          <h4>{ a[0] } <span onClick={ goodCount }>👍</span> {good} </h4>
           <p>2월 17일 발행</p>
         </div>
 
         <div className="list">
-          <h4>{ a[1] }</h4>
+          <h4>{ a[1] } <span onClick={ changeName }>[제목 변경]</span></h4>
           <p>2월 17일 발행</p>
         </div>
        
